ac01b79c82b4bab7c1bd40ecb3556c53f1e6955d
Jacob Lockhart <procoder165@gmail.com>
2022-09-14 19:13:34 -0400

    able to click on note to bring up note

--- routes/api.js
+++ routes/api.js
@@ -3,14 +3,13 @@ const api = express.Router();
 const fs = require("fs")
 
 
-api.get("/", (req, res, next) =>{
+api.get("/notes", (req, res, next) =>{
     fs.readFile("./db/db.json", "utf8", (err, data) =>{
         if(err){
             console.error(err);
         }
         else{
             const parsed = JSON.parse(data);
-    
             res.json(parsed);
         }
     })
@@ -18,26 +17,41 @@ api.get("/", (req, res, next) =>{
     
     const notes = [];
     
-    api.post("/", (req, res, next) =>{
+    api.post("/notes", (req, res, next) =>{
+
         fs.readFile("./db/db.json", "utf8", (err, data) =>{
             if(err){
                 console.error(err);
             }
             else{
                 const parsed = JSON.parse(data);
-                parsed.push(req.body);
+                const {title, text} = req.body;
+                const newNote = {
+                    title, 
+                    text, 
+                    id: parsed.length + 1,
+                }
+                parsed.push(newNote);
     
                 fs.writeFile("./db/db.json", JSON.stringify(parsed, null, 4), (err) => err ? console.error(err) : console.log("success"));
-                
             }
         })
-    
-    
-    
         next();
     })
     
-    api.delete("/", (req, res, next) =>{
+    api.delete("/notes", (req, res, next) =>{
+        fs.readFile("./db/db.json", "utf8", (err, data) =>{
+            if(err){
+                console.error(err);
+            }
+            else{
+                const parsed = JSON.parse(data);
+
+                for(note of notes){
+                    
+                }
+            }
+        })
         next();
     })
     
